a19fd011745bbf37014682491d56c462ade4e099
Clark Walker <303walker@gmail.com>
2017-11-19 16:40:06 -0700

    update

--- migrations/20171119155402_player_score.js
+++ migrations/20171119155402_player_score.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTable('player_score', (table) => {
+      table.increments('id').primary();
+      table.string('name');
+      table.integer('score');
+    })
+  ]);
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.dropTable('player_score')
+  ]);
 };
--- seeds/player_score.js
+++ seeds/player_score.js
@@ -5,9 +5,21 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('player_score').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {
+          // id: 1,
+          name: 'clark',
+          score: 55
+        },
+        {
+          // id: 2,
+          name: 'bob',
+          score: 234
+        },
+        {
+          // id: 3,
+          name: 'fred',
+          score: 76
+        }
       ]);
     });
 };
