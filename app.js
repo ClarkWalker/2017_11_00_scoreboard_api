c3238b76e73c33419f1dec8addf1917dfaf13600
Clark Walker <303walker@gmail.com>
2017-11-19 16:55:57 -0700

    update

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+
+// get and use routes
+const apiRoute = require('./routs/api.js')
+// here's your sign
+app.listen(port, () => {
+  console.log('Hi there gamer');
+});
---
+++ database/queries.js
@@ -0,0 +1,1 @@
+const db = ('./connection.js');
